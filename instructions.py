13fba3327b95dab1d65bb38b0014ba00ab0861ef
Awaismaz <awais_mazahir@hotmail.com>
2023-02-14 20:23:00 +0500

    Update Instructions 2

--- instructions.py
+++ instructions.py
@@ -14,6 +14,7 @@ Submission Method:
 3. Add a new file with your own name in the format firstname_lastname.py
 4. The structure of code is present in awais_mazahir.py. Copy the code to newly created file and build your function
 5. Once results are correct, push your code to your github account and initiate a pull request by clicking on "Contribute" Button
+6. Perform the changes in main branch only.
 
 Code Requirements:
 
