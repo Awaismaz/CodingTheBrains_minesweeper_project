c072f160499089af5c6af06fba4124f7dcc8541e
Awaismaz <awais_mazahir@hotmail.com>
2023-02-14 20:18:56 +0500

    Initial Commit

---
+++ awais_mazahir.py
@@ -0,0 +1,29 @@
+def minesweeper(A):
+    #code that coverts the test matrix into desired output
+    return A
+
+if __name__=="__main__":
+    
+    test = ["XOOXXXOO", 
+            "OOOOXOXX", 
+            "XXOXXOOO", 
+            "OXOOOXXX", 
+            "OOXXXXOX", 
+            "XOXXXOXO", 
+            "OOOXOXOX", 
+            "XOXXOXOX"]
+
+    """Desired Output is as under
+    
+    ["X11XXX32", 
+     "3335X5XX", 
+     "XX3XX554", 
+     "3X556XXX", 
+     "24XXXX6X", 
+     "X3XXX5X3", 
+     "245X6X5X", 
+     "X2XX4X4X"]
+    """
+
+    print(minesweeper(test))
+
---
+++ instructions.py
@@ -0,0 +1,31 @@
+"""
+This is the first project being assigned in AI enabled application development training by Coding The Brains.
+
+The project requires detailed understanding of Loops, Conditions, Lists, Git and Github. Students are advised to
+complete python crash course along with github training (https://youtu.be/2C4GWXVv55g) to be able to successfully execute the project.
+
+Submission Method:
+
+1. Students are required to create their github account and FORK the following git repository:
+    https://github.com/Awaismaz/minesweeper_project.git
+
+2. Watch https://youtu.be/2C4GWXVv55g to understand the Forking Process if you are unable to understand STEP 1
+
+3. Add a new file with your own name in the format firstname_lastname.py
+4. The structure of code is present in awais_mazahir.py. Copy the code to newly created file and build your function
+5. Once results are correct, push your code to your github account and initiate a pull request by clicking on "Contribute" Button
+
+Logic Building:
+
+1. You are required to create logic of minesweeper game. 
+2. Input received by your function will be will be a VARIABLE SIZED List.
+3. Every location where mine is present 'X' (alphabet) is marked. 'O' (alphabet) is marked where mine is not present
+4. For each location, you need to find, how many mines are present in neighbouring cells
+5. If mine is present in a cell, the output of the cell remains 'X'
+6. If mine is not present in a cell, its value will be replaced by a number depicting the mines present in 8 neighbouts
+7. Desired output of the function for given test input is shown in awais_mazahir.py file
+8. Your function will be tested against hidden input to verify that your code i
+
+Good Luck!!
+
+"""
