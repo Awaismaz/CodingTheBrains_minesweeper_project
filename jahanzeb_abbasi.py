1c7252db5c175ea16d957f7117cd1a05146ecefb
Jahanzeb <jahanzebabbasi49@gmail.com>
2023-02-17 21:28:07 +0500

    Removed the copy variable

--- jahanzeb_abbasi.py
+++ jahanzeb_abbasi.py
@@ -1,39 +1,37 @@
 def minesweeper(A):
 
-    copy = A
-    
     # Insert top & bottom padding rows
     padding = 'P' * len(A[0])
-    copy.insert(0, padding)
-    copy.append(padding)
+    A.insert(0, padding)
+    A.append(padding)
 
     # Insert padding to each row
-    for i in range(0, len(copy)):
-        copy[i] = "P" + copy[i] + "P"
+    for i in range(0, len(A)):
+        A[i] = "P" + A[i] + "P"
 
     #Iterate through Rows and Characters
-    for row in range(1, len(copy)-1):
-        for char in range(1, len(copy[0])-1):
-            if copy[row][char] != 'X' and copy[row][char] != 'P':
+    for row in range(1, len(A)-1):
+        for char in range(1, len(A[0])-1):
+            if A[row][char] != 'X' and A[row][char] != 'P':
                 counter = 0
 
                 # Checking for Mines
                 for i in range(row-1, row+2):
                     for j in range(char-1,char+2):
-                        if copy[i][j] == 'X':
+                        if A[i][j] == 'X':
                             counter += 1
 
                 # Replaces 0's with count of mines
-                copy[row] = copy[row][:char] + str(counter) + copy[row][char+1:]
+                A[row] = A[row][:char] + str(counter) + A[row][char+1:]
     
     # Removes Padding
-    for i in range(0, len(copy)):
-        copy[i] = copy[i][1:len(copy[i])-1]
+    for i in range(0, len(A)):
+        A[i] = A[i][1:len(A[i])-1]
 
-    copy.pop()
-    copy.remove(padding)
+    A.pop()
+    A.remove(padding)
 
-    return copy
+    return A
 
 if __name__=="__main__":
     
